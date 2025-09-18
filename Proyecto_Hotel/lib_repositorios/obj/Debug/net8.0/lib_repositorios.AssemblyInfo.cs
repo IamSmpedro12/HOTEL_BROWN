aab85e1e7c69e5686c56f8464f0992e6d5d9e4c1
IamSmpedro12 <smpedro87@gmail.com>
2025-09-17 21:27:42 -0500

    implementaciones1

--- Proyecto_Hotel/lib_repositorios/obj/Debug/net8.0/lib_repositorios.AssemblyInfo.cs
+++ Proyecto_Hotel/lib_repositorios/obj/Debug/net8.0/lib_repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a55b20e6eda5604c9e9986fea537360b94dc41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd460bb9a77ed44cf1fb898477e216a91db5b1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
